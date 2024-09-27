0ed7e4c336f2001b942a5fd4d18100340b6ab3e3
Umerov Kairat <qwzxcemperor@gmail.com>
2024-09-27 16:48:36 +0600

    fix problem of flexing boxes

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,38 +1,51 @@
-let userCreateThisPost = false;
-
 async function getPublications() {
-
-    let response = await fetch("/publications/getPublications", createRequest())
-
-    $("#comment").html("");
-
-    let res = await response.json();
-    let data = document.getElementById('form-publication');
-    data.innerHTML = '';
-    for (let i = 0; i < res.length; i++) {
-        let newElement = document.createElement('div');
-        let isLike = '';
-        if (res[i].like) {
-            isLike = '<div class="d-flex justify-content-end me-3">' +
-                '<button type="button" class="btn btn-outline-danger col-2">' +
-                '  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">' +
-                '     <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path>' +
-                '  </svg>' +
-                '</button>' +
-                '</div>';
+    try {
+        let response = await fetch('/publications/getPublications');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
-        newElement.innerHTML =
-            '   <div onclick="post_info(\'' + res[i].id + '\', true)" class="card opacity-75 border-0 bg-white h-100 mt-3 py-5 ">' +
-            '      <div class="card-body border-top border-3 mx-3 px-0 d-flex flex-wrap justify-content-center  align-content-around flex-column">' +
-            '           <img src="/publications/download/' + res[i].id + '" class="card-img opacity-100"\n' +
-            '                                 style="width: 45%;" alt="image">' +
-            '           <p class="card-title text-center mt-2 fst-italic fs-5">' + res[i].description + '</p>' +
-            '      </div>' +
-            '      <input type="hidden" id="like_id">' +
-                   isLike +
-            '</div>';
-        data.appendChild(newElement);
+        let publications = await response.json();
+        displayPublications(publications);
+    } catch (error) {
+        console.error('Ошибка при получении публикаций:', error);
     }
 }
 
-getPublications();
+function displayPublications(publications) {
+    let dataContainer = document.getElementById('form-publication');
+    dataContainer.innerHTML = ''; // Очистка предыдущих публикаций
+
+    if (publications.length === 0) {
+        dataContainer.innerHTML = '<p>Нет доступных публикаций для отображения.</p>';
+        return;
+    }
+
+    publications.forEach(publication => {
+        let publicationElement = document.createElement('div');
+        publicationElement.className = "card opacity-75 border-0 bg-white h-100 mt-3 py-5";
+        publicationElement.onclick = function() {
+            post_info(publication.id, true);
+        };
+
+        let cardBody = document.createElement('div');
+        cardBody.className = "card-body border-top border-3 mx-3 px-0 d-flex flex-wrap justify-content-center align-content-around flex-column";
+
+        let img = document.createElement('img');
+        img.src = "/publications/download/" + publication.id;
+        img.className = "card-img opacity-100";
+        img.style.width = "45%";
+        img.alt = "image";
+
+        let title = document.createElement('p');
+        title.className = "card-title text-center mt-2 fst-italic fs-5";
+        title.innerText = publication.description;
+
+        cardBody.appendChild(img);
+        cardBody.appendChild(title);
+        publicationElement.appendChild(cardBody);
+        dataContainer.appendChild(publicationElement);
+    });
+}
+
+// Вызов функции при загрузке страницы
+document.addEventListener('DOMContentLoaded', getPublications);
